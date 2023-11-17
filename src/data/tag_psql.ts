64accb69b83aaaf50034919e68a66a7084713c2b
NightenDushi <nathan.guilhot@gmx.fr>
2023-11-17 11:55:33 +0100

    Copy paste fixes

--- src/data/tag_psql.ts
+++ src/data/tag_psql.ts
@@ -3,26 +3,26 @@ import { ITags } from '../@types/tag'
 import pool from '../pool'
 
 async function get(id:number):Promise<ITags>{
-    const result = await pool.query('SELECT * FROM tickets WHERE id = $1', [id]);
+    const result = await pool.query('SELECT * FROM tags WHERE id = $1', [id]);
     return result.rows[0];
 }
 async function getAll():Promise<ITags[]>{
-    const result = await pool.query('SELECT * FROM tickets', [])
+    const result = await pool.query('SELECT * FROM tags', [])
     return result.rows
 }
 
 async function set(id:number, value:ITags):Promise<ITags>{
-    await pool.query(`UPDATE tickets SET (text, color) = ($1, $2) WHERE id = $3`,
+    await pool.query(`UPDATE tags SET (text, color) = ($1, $2) WHERE id = $3`,
                     [value.text, value.color, id]);
     return get(id)
 }
 async function Add(value:ITags):Promise<ITags>{
-    await pool.query(`INSERT INTO tickets (text, color) VALUES ($1, $2)`, [value.text, value.color]);
-    const result = await pool.query('SELECT * FROM tickets ORDER BY id DESC LIMIT 1');
+    await pool.query(`INSERT INTO tags (text, color) VALUES ($1, $2)`, [value.text, value.color]);
+    const result = await pool.query('SELECT * FROM tags ORDER BY id DESC LIMIT 1');
     return result.rows[0]
 }
 async function Delete(id:number):Promise<ITags[]>{
-    await pool.query('DELETE FROM tickets WHERE id = $1', [id]);
+    await pool.query('DELETE FROM tags WHERE id = $1', [id]);
     return getAll()
 }
 
